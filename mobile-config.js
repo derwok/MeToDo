ae63278cce0c9f813ff6635cfb91c58e3239edc7
Wolfram Esser <github@derwok.de>
2015-03-07 19:50:55 +0100

    Initial commit

---
+++ mobile-config.js
@@ -0,0 +1,17 @@
+App.info({
+  name: 'WOK SiToDo',
+  description: 'A simple ToDo App',
+  version: '0.0.1'
+});
+
+App.icons({
+  'iphone': 'resources/icons/icon-60.png',
+  'iphone_2x': 'resources/icons/icon-60@2x.png'
+});
+
+// App.launchScreens({
+//   'iphone': 'resources/splash/Default~iphone.png',
+//   'iphone_2x': 'resources/splash/Default@2x~iphone.png',
+//   'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
+// });
+
---
+++ simple-todos.js
@@ -0,0 +1,131 @@
+// simple-todos.js
+
+// Meteor.startup(function () {...}
+
+Tasks = new Mongo.Collection("tasks");
+
+////////// Client Code! /////////
+if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
+
+  Template.body.helpers({
+
+    // tasks: [
+    //       { text: "This is task 1" },
+    //       { text: "This is task 2" },
+    //       { text: "This is task 3" }
+    //     ],
+
+    // tasks: function () {
+    //   return Tasks.find({}, {sort: [["createdAt", "desc"]]})
+    // },
+
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    },
+
+    usernameUpperPlusS: function () {
+      if (!Meteor.user().username || Meteor.user().username.length < 3) {
+        return "";
+      }
+      else {
+        return Meteor.user().username.toUpperCase()+"s ";
+      }
+    }
+  });
+
+
+  //////////////////////////////////////////////////////////////
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      console.log(event);
+
+      var text = event.target.text.value;
+      Meteor.call("addTask", text);       // async server & client (latency compensation)
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    },
+
+    "change .switch input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+    // "change .hide-completed input": function (event) {
+    //   Session.set("hideCompleted", event.target.checked);
+    // }
+  });  // Template.body.events
+
+
+  //////////////////////////////////////////////////////////////
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Meteor.call("setChecked", this._id, ! this.checked);  // async server & client (latency compensation)
+    },
+
+    "click .delete": function () {
+      Meteor.call("deleteTask", this._id);
+    }
+  });  // Template.task.events
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+}  // Meteor.isClient
+
+
+
+
+////////// Server Code! /////////
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function () {
+    return Tasks.find({ owner: this.userId });
+  });
+}
+
+
+
+////////// Client & Server Code! /////////
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
+
