e40a4f3698a775c868e601f73d91feda6eebde16
Wolfram Esser <github@derwok.de>
2015-10-12 08:59:17 +0200

    Drag'n'Drop with blue indicator line. But blue line prevented on completed tasks. Drop knows source and destination Task._id

--- client/templates/taskdragdrop.js
+++ client/templates/taskdragdrop.js
@@ -4,37 +4,63 @@
 
 
 Template.task.MT_drag = function (evt) {
-    console.log("DRAGGED!");
-    evt.dataTransfer.setData("text", evt.target.id);
+    var tr = $(evt.target).closest("tr");
+    var draggedTaskID = tr.attr('MTTaskID');
+    evt.dataTransfer.setData("MTTaskID", draggedTaskID);
+    console.log("DRAGGED!" + draggedTaskID);
     console.log(evt);
 };
 
+
 Template.task.MT_dragEnter = function (evt) {
-    evt.preventDefault();
-    console.log("DRAGENTER!");
     var tr = $(evt.target).closest("tr");
-    var targetTaskID = tr.attr('MTTaskID')
-    console.log("TR.MTTaskID="+tr.attr('MTTaskID'));
-    Session.set("CurrentDragTargetTaskID", targetTaskID)
+    var targetTaskID = tr.attr('MTTaskID');
+    if (! Session.get("CurrentDragTargetEnterCount")) {
+        Session.set("CurrentDragTargetEnterCount", 0)
+    }
+    Session.set("CurrentDragTargetEnterCount", Session.get("CurrentDragTargetEnterCount")+1);
+    Session.set("CurrentDragTargetTaskID", targetTaskID);
+
+    //console.log("DRAGENTER!" + Session.get("CurrentDragTargetEnterCount"));
+    //console.log(evt);
+    //console.log("TR.MTTaskID="+tr.attr('MTTaskID'));
 };
+
 
 Template.task.MT_dragOver = function (evt) {
-    evt.preventDefault();
-    return false;
+    var tr = $(evt.target).closest("tr");
+    var draggable = tr.attr('draggable');
+    // console.log("DRAGOVER:"+draggable);
+
+    // We dont' allow to drag over a <tr> that is not draggable itself.
+    // e.g. we dont allow to drop on checked (finished) tasks
+    if (draggable && draggable == "true") {
+        evt.preventDefault();
+        return false;
+    }
+    return true;
 };
 
+
 Template.task.MT_dragLeave = function (evt) {
-    evt.preventDefault();
-    console.log("DRAGLEAVE!");
-    Session.set("CurrentDragTargetTaskID", undefined)
+    Session.set("CurrentDragTargetEnterCount", Session.get("CurrentDragTargetEnterCount")-1);
+    if (Session.get("CurrentDragTargetEnterCount") <= 1) {
+        Session.set("CurrentDragTargetEnterCount", 0);
+        Session.set("CurrentDragTargetTaskID", undefined);
+    }
+    //console.log("DRAGLEAVE!" + Session.get("CurrentDragTargetEnterCount"));
+    //console.log(evt);
 };
 
 
 Template.task.MT_drop = function (evt) {
-    console.log("DROPPED!");
     evt.preventDefault();
-    Session.set("CurrentDragTargetTaskID", undefined)
-    // var data = evt.dataTransfer.getData("text");
+    Session.set("CurrentDragTargetEnterCount", 0);
+    Session.set("CurrentDragTargetTaskID", undefined);
+
+    var tr = $(evt.target).closest("tr");
+    var targetTaskID = tr.attr('MTTaskID');
+    var draggedTaskID = evt.dataTransfer.getData("MTTaskID");
+    console.log("DROPPED! " + draggedTaskID + " => "+ targetTaskID);
     console.log(evt);
-    // console.log("Data"+data);
 };
